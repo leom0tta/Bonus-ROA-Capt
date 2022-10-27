89773e45f48a1103bbfea7a441bf629ddd463bc5
leom0tta <leonardogmotta@poli.ufrj.br>
2022-10-27 18:41:47 -0300

    teste

--- Bases de Performance/gera_receitas_por_indicador.py
+++ Bases de Performance/gera_receitas_por_indicador.py
@@ -1,12 +1,12 @@
 import pandas as pd
 import sys
-sys.path.insert(1, r'.\Funções')
-from funcoes import gera_excel
+sys.path.insert(1, r".\Funções")
+from classes import Get
 
 meses= [['Janeiro', '310122'], ['Fevereiro', '250222'], ['Março', '310322'], ['Abril', '290422'], ['Maio', '310522'], ['Junho', '300622']]
 
-assessores_leal = pd.read_excel (r"bases_dados\Assessores leal_Pablo.xlsx")
-suitability = pd.read_excel (r"bases_dados\Suitability.xlsx")
+assessores_leal = Get.assessores()
+suitability = Get.suitability()
 
 suitability['CodigoBolsa'] = suitability['CodigoBolsa'].astype(str)
 assessores_leal['Código assessor'] = assessores_leal['Código assessor'].astype(str)
@@ -14,11 +14,10 @@ assessores_leal['Código assessor'] = assessores_leal['Código assessor'].astype
 writer = pd.ExcelWriter('teste_receitas_2022.xlsx' , engine='xlsxwriter', datetime_format = 'dd/mm/yyyy')
 
 for mes, data_hoje in meses:
-
-    comissao = pd.read_csv(r"Comissões\Receitas\Bases SplitC\dados_comissão_" + str.lower(mes.replace('ç', 'c')) + ".csv",sep=';', decimal=',')
+    comissao = Get.receitas(mes)
     comissao['Cliente'] = comissao['Cliente'].astype(str)
 
-    clientes_rodrigo = pd.read_excel(r"Clientes Rodrigo\Clientes Rodrigo " + mes + ".xlsx")
+    clientes_rodrigo = Get.clientes_rodrigo(mes)
     clientes_rodrigo['Conta'] = clientes_rodrigo['Conta'].astype(str)
 
     mask_ajustes = comissao['Categoria'] == "Ajuste"
