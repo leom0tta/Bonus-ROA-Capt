d8774acebf58a38da89f50d2fae8b643645b0a1c
leom0tta <81717275+leom0tta@users.noreply.github.com>
2022-10-27 19:55:20 -0300

    Update datasets.py

--- ModeloBônusCaptação/datasets.py
+++ ModeloBônusCaptação/datasets.py
@@ -4,7 +4,7 @@ import numpy as np
 
 assessores = Get.assessores()
 
-path = r'C:\Users\Leonardo\Dropbox\Fatorial\Inteligência\Codigos\ModeloBônusCaptação\BD\métricas_captação.xlsx'
+path = r'métricas_captação.xlsx'
 
 df = pd.read_excel(path, sheet_name='df')
 price = pd.read_excel(path, sheet_name='price')
