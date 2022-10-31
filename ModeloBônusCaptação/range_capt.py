2675ff98332c64da350f66da0bfa1f88c0e0bea5
leom0tta <leonardogmotta@poli.ufrj.br>
2022-10-31 11:52:46 -0300

    Limpando o Formato

--- ModeloBônusCaptação/range_capt.py
+++
@@ -1,7 +0,0 @@
-import numpy as np 
-
-range_roa = np.linspace(0.0, 0.02, 21)
-range_capt_bonus = np.linspace(0, 0.05, 11)
-
-range_roa = range_roa.round(3)
-range_capt_bonus = range_capt_bonus.round(3)
